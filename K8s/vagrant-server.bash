346f6cffdac6601926a7642bcf18dbb7e5c05ad2
Mustafa Mohammed <128603198+mustafa-mohammed04@users.noreply.github.com>
2023-05-14 00:40:19 +0300

    vagrant-file.bash

---
+++ K8s/vagrant-server.bash
@@ -0,0 +1,59 @@
+## Vagrant-server
+
+``` bash
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  
+  config.vm.box = "ubuntu/jammy64"
+
+
+ 
+  config.vm.hostname = "server"
+  config.vm.network "public_network", ip: "192.168.56.12", hostname: true
+
+
+  config.vm.provider :virtualbox do |vb|
+
+
+   
+    vb.customize [
+      'modifyvm', :id,
+      '--memory', '2048',
+      '--cpus', '2'
+    ]
+  end
+
+ 
+end
+```
+
+## Vagrant-agent
+
+``` bash
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  
+  config.vm.box = "ubuntu/jammy64"
+
+
+ 
+  config.vm.hostname = "agent"
+  config.vm.network "public_network", ip: "192.168.56.13", hostname: true
+
+
+  config.vm.provider :virtualbox do |vb|
+
+
+   
+    vb.customize [
+      'modifyvm', :id,
+      '--memory', '1024',
+      '--cpus', '1'
+    ]
+  end
+
+ 
+end
+```
